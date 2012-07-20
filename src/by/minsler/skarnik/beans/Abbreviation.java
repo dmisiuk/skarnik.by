0382f8488e6faa7d0d29fb4a378800a0c0b1e7de
Dzmitry Misiuk <minsler.by@gmail.com>
2012-07-20 22:16:50 +0300

    Added bean for abbreviation

--- src/by/minsler/skarnik/beans/Abbreviation.java
+++ src/by/minsler/skarnik/beans/Abbreviation.java
@@ -1,5 +1,36 @@
 package by.minsler.skarnik.beans;
 
 public class Abbreviation{
-	
+
+	private int id;
+	private String shortName;
+	private String fullName;
+
+	public Abbreviation(){
+	}
+
+	public void setShortName(String shortName){
+		this.shortName = shortName;
+	}
+
+	public void setFulltName(String fullName){
+		this.fullName = fullName;
+	}
+
+	public void setId(int id){
+		
+	}
+
+	public String getShortName(){
+		return shortName;
+	}
+
+	public String getFullName(){
+		return fullName;
+	}
+
+	public int getId(){
+		return id;
+	}
+
 }
